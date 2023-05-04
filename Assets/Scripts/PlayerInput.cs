c4a21051379a082ddd4cb6a7c100880fa25eed17
ashwicket <124555370+ashwicket@users.noreply.github.com>
2023-05-05 00:22:26 +0530

    Movement and Camera

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    private enum CurrentPlayer { player1, player2 };
+
+    private PlayerControls playerControls;
+    private float moveInput;
+    private float rotationInput;
+
+    [SerializeField]
+    CurrentPlayer player;
+
+    public float MoveInput { get => moveInput; }
+    public float RotationInput { get => rotationInput; }
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        InputAction playerMove;
+        InputAction playerRotate;
+
+        if (player == CurrentPlayer.player1)
+        {
+            //Assign Controls for Player 1
+            playerMove = playerControls.Player1.Move;
+            playerRotate = playerControls.Player1.Rotate;
+        }
+        else
+        {
+            //Assign Controls for Player 2
+            playerMove = playerControls.Player2.Move;
+            playerRotate = playerControls.Player2.Rotate;
+        }
+
+        //Assign Move Value
+        playerMove.performed += _ => Move(_.ReadValue<float>());
+        playerMove.canceled += _ => Move(_.ReadValue<float>());
+
+        //Assign Rotate Value
+        playerRotate.performed += _ => Rotate(_.ReadValue<float>());
+        playerRotate.canceled += _ => Rotate(_.ReadValue<float>());
+    }
+
+    private void Move(float value)
+    {
+        moveInput = value;
+    }
+
+    private void Rotate(float value)
+    {
+        rotationInput = value;
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private const float rad = Mathf.Deg2Rad;
+
+    [SerializeField]
+    PlayerInput playerInput;
+    [SerializeField]
+    private float moveSpeed = 5.0f;
+    [SerializeField]
+    private float rotateSpeed = 24.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Speed() * MoveDirection();
+    }
+
+    private float Speed()
+    {
+        if (playerInput)
+        {
+            return moveSpeed * Time.deltaTime * playerInput.MoveInput;
+        }
+        else
+        {
+            return 0.0f;
+        }
+    }
+
+    private Vector3 MoveDirection()
+    {
+        if (playerInput)
+        {
+            float newAngle = rotateSpeed * Time.deltaTime * playerInput.RotationInput + transform.eulerAngles.y;
+            transform.eulerAngles = new Vector3(0.0f, newAngle, 0.0f);
+
+            float angle = transform.eulerAngles.y * rad;
+            float front = -Mathf.Sin(angle);
+            float side = Mathf.Cos(angle);
+
+            return new Vector3(side, 0.0f, front);
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+}
